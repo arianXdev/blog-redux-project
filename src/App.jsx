492cdac119ee9e9ef4415cc0c814e3d6a10e8707
Arian Hosseini <arianhosseini@outlook.com>
2023-05-10 11:24:00 +0330

    feat: created <Navbar /> component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 const App = () => {
-	return <div>Hello, World!</div>;
+	return (
+		<main className="App">
+			<Navbar />
+		</main>
+	);
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<h1 className="navbar__title">The Redux Blog Project</h1>
+			<hr />
+		</nav>
+	);
+};
+
+export default Navbar;

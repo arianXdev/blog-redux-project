73b9f56c11cf2e0d81e623d6456eee39af2d2388
Arian Hosseini <arianhosseini@outlook.com>
2023-05-10 11:52:25 +0330

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import PostsList from "./features/posts/PostsList";
 import "./App.css";
 
 const App = () => {
 	return (
 		<main className="App">
 			<Navbar />
+
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<>
+							<PostsList />
+						</>
+					}
+				/>
+			</Routes>
 		</main>
 	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
@@ -11,7 +12,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
 );

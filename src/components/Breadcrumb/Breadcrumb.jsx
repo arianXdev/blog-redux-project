3796f72d181917c2e9fcb9a5b7d9576095258938
Arian Hosseini <arianhosseini@outlook.com>
2023-05-10 21:08:21 +0330

    removed useless import

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
 import "./Breadcrumb.css";
-import { useEffect } from "react";
 
 const Breadcrumb = () => {
 	const { pathname } = useLocation();

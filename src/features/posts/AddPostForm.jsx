71e111bf3ae960311c09b7c183c44c93bb8552be
Arian Hosseini <arianhosseini@outlook.com>
2023-05-11 11:18:27 +0330

    chore: removed useless import

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
 import { postAdded } from "./postsSlice";
 
 import "./AddPostForm.css";

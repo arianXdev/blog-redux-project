5931ef5e23261b7cebed0bddfb7c84a6d56fea67
Arian Hosseini <arianhosseini@outlook.com>
2023-05-11 19:49:39 +0330

    feat: created <ReactionButtons /> component

---
+++ src/features/posts/ReactionButtons.jsx
@@ -0,0 +1,29 @@
+import "./ReactionButtons.css";
+
+import { useDispatch } from "react-redux";
+
+import { reactionAdded } from "./postsSlice";
+
+const ReactionButtons = ({ post }) => {
+	const dispatch = useDispatch();
+
+	const reactionEmoji = {
+		thumbsUp: "👍",
+		hooray: "🎉",
+		heart: "❤️",
+		rocket: "🚀",
+		eyes: "👀",
+	};
+
+	const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+		return (
+			<button key={name} type="button" className="reactions__btn" onClick={() => dispatch(reactionAdded({ postId: post.id, reaction: name }))}>
+				{emoji} {post.reactions[name] === 0 ? null : post.reactions[name]}
+			</button>
+		);
+	});
+
+	return <section className="reactions">{reactionButtons}</section>;
+};
+
+export default ReactionButtons;

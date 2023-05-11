bfce935115dc37a8bd8a3bc2f5e4c9273287f020
Arian Hosseini <arianhosseini@outlook.com>
2023-05-11 07:56:18 +0330

    feat: created postUpdated reducer function

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -13,11 +13,20 @@ const postsSlice = createSlice({
 			// action.payload = newPost
 			state.push(action.payload);
 		},
+
+		postUpdated: (state, action) => {
+			const { id, title: newTitle, content: newContent } = action.payload;
+			const existingPost = state.find((post) => post.id === id);
+			if (existingPost) {
+				existingPost.title = newTitle;
+				existingPost.content = newContent;
+			}
+		},
 	},
 });
 
 // When we write the postAdded reducer function, createSlice will automatically generate an action creator func with the same name.
 // We can export that action creator and use it in our UI components to dispatch the action when the user clicks "Save Post".
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, postUpdated } = postsSlice.actions;
 
 export default postsSlice.reducer;
